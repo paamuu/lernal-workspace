446f662153358959b597a1deb000633bfbb49019
pamu <pamu1976@outlook.com>
2024-08-20 21:56:58 +0800

    动态解析npm包名称，删除lerna bootstrap命令解析依赖

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,19 +2,51 @@
 import { Command } from 'commander';
 import * as path from 'path';
 import { readdirSync } from 'node:fs';
+import { spawn, sync } from 'cross-spawn';
 
 const program = new Command();
 console.log('test');
+
 //  动态获取其他的内容来添加test
-function register() {
-  const pluginPath = path.resolve(__dirname, '../plugins');
-  const files = readdirSync(pluginPath);
-  files.forEach(file => {
-    if (!file.endsWith('.d.ts')) {
-      import(path.resolve(pluginPath, file)).then((_module) => {
-        program.addCommand(_module.program);
-      });
-    }
+async function register() {
+  // const child = sync('npm search muilk', { stdio: 'pipe' });
+  const { plugins, toolkits } = await getPackages();
+  console.log(toolkits, 'took');
+  // try {
+  //   sync(`npm install ${toolkits.join(' ')}`, { stdio: 'inherit' });
+  // } catch (error) {
+  //   console.error(`Failed to install package  ${error.message}`);
+  //   process.exit(1);
+  // }
+  // 将所有依赖包添加到package中，然后重新安装
+  // const pluginPath = path.resolve(__dirname, '../plugins');
+  // 只知道包的名称
+  // const files = readdirSync(pluginPath);
+  // files.forEach(file => {
+  //   if (!file.endsWith('.d.ts')) {
+  //     import(path.resolve(pluginPath, file)).then((_module) => {
+  //       program.addCommand(_module.program);
+  //     });
+  //   }
+  // });
+}
+
+/**
+ * 根据npm接口获取包
+ */
+async function getPackages() {
+  return fetch('https://registry.npmjs.org/-/v1/search?text=scope:muilk').then(res => res.json()).then(result => {
+    const packages = result.objects || [];
+    const toolkits = packages.filter((item: any) => {
+      return item.package.name.startsWith('@muilk/toolkit-');
+    })?.map((p: any) => p.package.name);
+    const plugins = packages.filter((item: any) => {
+      return item.package.name.startsWith('@muilk/plugin-');
+    })?.map((p: any) => p.package.name);
+    return {
+      toolkits,
+      plugins,
+    };
   });
 }
 
