0e7000f8ad62041f8c708b047240816fa4328a19
pamu <pamu1976@outlook.com>
2024-08-27 22:42:19 +0800

    添加schematic-cli来执行schematic

---
+++ packages/toolkit-angular/lib/schematic.runner.ts
@@ -0,0 +1,36 @@
+import { ChildProcess, spawn, SpawnOptions } from 'child_process';
+
+function execute() {
+  const options: SpawnOptions = {
+    cwd: process.cwd(),
+    stdio: 'inherit',
+    shell: true,
+  };
+  const schematic = getSchematicExecutePath();
+  const child: ChildProcess = spawn(
+    `node`,
+    [schematic],
+    options,
+  );
+  child.on('close', (code) => {
+    if (code === 0) {
+      console.log('执行成功');
+    } else {
+      console.log('执行失败');
+    }
+  });
+}
+
+/**
+ * 获取schematic-cli的执行文件所在目录
+ */
+function getSchematicExecutePath() {
+  return require.resolve(
+    '@angular-devkit/schematics-cli/bin/schematics.js',
+    { paths: this.getModulePaths() },
+  );
+}
+
+function getModulePaths() {
+  return module.paths;
+}
